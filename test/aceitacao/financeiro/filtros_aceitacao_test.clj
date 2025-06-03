631eef7c3b741848287e9ec3efee7a51077db112
Rodrigo Peleias <rodrigo.peleias@rodrigo.peleias>
2025-06-03 10:47:57 -0300

    test: added acceptance test for transactions filtering

---
+++ test/aceitacao/financeiro/filtros_aceitacao_test.clj
@@ -0,0 +1,37 @@
+(ns aceitacao.financeiro.filtros-aceitacao-test
+  (:require [cheshire.core :as json]
+            [financeiro.db :as db]
+            [midje.sweet :refer :all]
+            [unitarios.financeiro.auxiliares :refer :all]))
+
+(def transacoes-aleatorias
+  '({:valor 7.0M :tipo "despesa"}
+    {:valor 88.0M :tipo "despesa"}
+    {:valor 106.0M :tipo "despesa"}
+    {:valor 8000.0M :tipo "receita"}))
+
+(against-background [(before :facts [(iniciar-servidor porta-padrao) (db/limpar)])
+                     (after :facts (parar-servidor))]
+
+  (fact "Não existem receitas" :aceitacao
+        (json/parse-string (conteudo "/receitas") true) => {:transacoes '()})
+
+  (fact "Não existem despesas" :aceitacao
+        (json/parse-string (conteudo "/despesas") true) => {:transacoes '()})
+
+  (fact "Não existem transações" :aceitacao
+        (json/parse-string (conteudo "/transacoes") true) => {:transacoes '()}))
+
+(against-background [(before :facts (doseq [transacao transacoes-aleatorias]
+                                      (db/registrar transacao)))
+                     (after :facts (db/limpar))]
+
+  (fact "Existem 3 despesas" :aceitacao
+        (count (:transacoes (json/parse-string (conteudo "/despesas") => true))) => 3)
+
+  (fact "Existe 1 receita" :aceitacao
+      (count (:transacoes (json/parse-string (conteudo "/receitas") => true))) => 1)
+
+  (fact "Existem 4 transações" :aceitacao
+        (count (:transacoes (json/parse-string (conteudo "/transacoes") => true))) => 4)
+)
